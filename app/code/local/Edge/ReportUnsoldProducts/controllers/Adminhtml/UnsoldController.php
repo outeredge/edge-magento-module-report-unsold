f17d1c5a4759a1b95bf147540600f84e265ac117
Victor Seager <vseager@gmail.com>
2015-07-09 13:16:22 +0100

    Fixed permissions for magento 1.9.2.0

--- app/code/local/Edge/ReportUnsoldProducts/controllers/Adminhtml/UnsoldController.php
+++ app/code/local/Edge/ReportUnsoldProducts/controllers/Adminhtml/UnsoldController.php
@@ -2,6 +2,11 @@
 
 class Edge_ReportUnsoldProducts_Adminhtml_UnsoldController extends Mage_Adminhtml_Controller_Action
 {
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('report/products/unsold');
+    }
+    
     protected function _initAction()
     {
         $this->loadLayout()
